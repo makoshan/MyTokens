c0cd9d99a25145988e9b5b22519d6d73dc597b3e
Mako Shan <imakoshan@gmail.com>
2026-02-12 22:23:05 +0800

    refactor: Move index.html to src/ directory

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import type { RollupOptions } from 'rollup'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'src',
   base: './',
+  publicDir: '../public', // Point to project root public dir if it exists/created later
   // Vite options tailored for Tauri to prevent too much magic
   clearScreen: false,
   server: {
@@ -17,6 +19,8 @@ export default defineConfig({
     },
   },
   build: {
+    outDir: '../dist', // Output to project root dist/
+    emptyOutDir: true,
     target: ['es2021', 'chrome100', 'safari13'],
     minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
     sourcemap: !!process.env.TAURI_DEBUG,
